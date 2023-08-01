42ab1ffcd7f72df0b1383b629d6b957096daa9b9
Niti9 <nnegi8298@gmail.com>
2023-08-01 17:06:55 +0530

    make some data inside object and use as spread operator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import Video from './components/video.js'
 import './App.css';
 function App() {
+
+  // creating object 
+  let obj = {
+    title: "React Js tutorial",
+    views: "999k",
+    time: "1 year ago",
+    channel: 'Coder Dost',
+
+  }
   return (
 
     <>
       <div className='App'>
         <div>Video</div>
-        <Video title="React Js tutorial"
-          views="10k"
-          time="1 year ago">
-        </Video>
+        {/* spread operator */}
+        <Video {...obj} > </Video>
+
         <Video title="Node Js tutorial"
           views="100k"
-          time="1 month ago">
+          time="1 month ago"
+          channel='Abhishek Rathore'
+          
+        >
+
         </Video>
         <Video title="MongoDB tutorial"
           views="140k"
-          time="5 month ago">
+          time="5 month ago"
+         >
+
         </Video>
       </div>
     </>
--- src/components/video.js
+++ src/components/video.js
@@ -1,15 +1,18 @@
 import './video.css';
 
-{/* destructured way instead of props*/ }
-function Video({ title, channel, views, time }) {
+/* destructured way instead of props*/
+// agar humne props mein default kisi chiz ko diya hai aur app.js mein value 
+//dena bhul gaye to ye default value apne aap usmein show hogi
+function Video({ title, channel = "CODER_DOST", views, time }) {
     return (
         <>
             <div className='container'>
-                <div className='pic'>
-                    <img src="https://picsum.photos/id/237/200/300" alt="Katherine Johnson" />
+                <div className='pic' > 
+
+                    <img src="https://picsum.photos/id/3/5000/3333" alt="Katherine Johnson" />
                 </div>
                 <div className='title'>{title}</div>
-                <div className='channel'>{channel} </div>
+                <div className='channel'>{channel} ✅ </div>
                 <div className='views'>{views}
                     <span>.</span> {time}
                 </div>
