67b1bfefdb8c44eb7cbe861556140d5f22e69bdb
Niti9 <nnegi8298@gmail.com>
2023-08-01 16:29:58 +0530

    multiple destructured props , css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import Video from './components/video.js'
+import './App.css';
 function App() {
   return (
 
     <>
-      <div>Hello</div>
-      {/* attributes in javascript used here as "PROPS" */}
-
-      {/* same component can be pass or use multiple times */}
-      <Video bgColor="Red" title="React Js tutorial"></Video>
-      <Video bgColor="Green" title="Node Js tutorial"></Video>
+      <div className='App'>
+        <div>Video</div>
+        <Video title="React Js tutorial"
+          views="10k"
+          time="1 year ago">
+        </Video>
+        <Video title="Node Js tutorial"
+          views="100k"
+          time="1 month ago">
+        </Video>
+        <Video title="MongoDB tutorial"
+          views="140k"
+          time="5 month ago">
+        </Video>
+      </div>
     </>
   )
 }
--- src/components/video.js
+++ src/components/video.js
@@ -1,15 +1,19 @@
 import './video.css';
 
-{/* destructured way instead of props*/}
-function Video({title,bgColor}) {
-   
-    let bg = 'dark';
+{/* destructured way instead of props*/ }
+function Video({ title, channel, views, time }) {
     return (
         <>
-            <img src="https://picsum.photos/id/237/200/300" alt="Katherine Johnson" />
-            
-            {/* inline css  or double curly braces used for CSS */}
-            <div className={bg} style={{ backgroundColor:bgColor}} >{title}</div>
+            <div className='container'>
+                <div className='pic'>
+                    <img src="https://picsum.photos/id/237/200/300" alt="Katherine Johnson" />
+                </div>
+                <div className='title'>{title}</div>
+                <div className='channel'>{channel} </div>
+                <div className='views'>{views}
+                    <span>.</span> {time}
+                </div>
+            </div>
         </>
     )
 }
