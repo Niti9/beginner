5b1f4d410a3d2f1594e15aeb9a7682704f1e0c35
Niti9 <nnegi8298@gmail.com>
2023-08-01 14:50:10 +0530

    chapter 1 components

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+         Hello this is starting of react 
+         <Demo></Demo>
     </div>
   );
 }
+
+function Demo() {
+  return (
+    <div className="App">
+     
+       we make two function and this function is take in app function as component 
+    </div>
+  );
+}
+
 
 export default App;
