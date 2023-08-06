309a354fec49930e179d162a1d9465be7b488590
Niti9 <nnegi8298@gmail.com>
2023-08-06 08:12:25 +0530

    some change in forms like try to add views and title

--- src/components/AddVideo.js
+++ src/components/AddVideo.js
@@ -1,14 +1,13 @@
 import { useState } from 'react';
-import videoDB from '../data/data.js';
 import './AddVideo.css'
 import video from './video.js';
 
 function AddVideo() {
-    
-    const [videos, setVideos] = useState({
-                time: '1 month ago',
-                channel: 'Coder Dost',
-                verified: true
+
+    const [video, setVideos] = useState({
+        time: '1 month ago',
+        channel: 'Coder Dost',
+        verified: true
     });
 
     function handleSubmit() {
@@ -17,14 +16,22 @@ function AddVideo() {
 
     function handleChange(e) {
         // view or target with the value show honge console mein
-        console.log(e.target.name, e.target.value);
-        setVideos({...video,
+        console.log(e.target.name, e.target.value); // output: title : "titleName"
+
+        //Yahan setVideos mein spread operator waala video 
+        //use state mein jo data diya hai usse waise hi lega
+        // aur title aur views change honge bas lekin
+        setVideos({
+            ...video,
             // is square bracket se pehle javascript samjhega ki
             //name pehle evaluate hoga phir key banega aur saamne 
             //waali value banegi
-           [e.target.name]: e.target.value
+            [e.target.name]: e.target.value
 
         })
+        // console.log(videos) karne par data next render mein dikhta rahega
+        //jaise agar title:"abc" likha hai to title le lega input pura 
+        //lekin record aur compelete data ke saath sirf title:"ab" hi show hoga
         console.log(video);
 
     }
@@ -34,22 +41,28 @@ function AddVideo() {
     return (
         // Adding form to add video by adding title, views
         <form>
-            <input type="text" name='title' onChange={handleChange} placeholder='title' />
-            <input type="text"name='views' onChange={handleChange} placeholder='views' />
+            <input type="text"
+                name='title'
+                onChange={handleChange}
+                placeholder='title' 
+                /> {video.title} 
+                {/*video.title se ye correct data dikhayega jaise abc diya hai to abc hi show hoga  */}
 
-            <button 
-            // new onclick function
-            onClick={handleSubmit}
+            <input type="text" name='views' onChange={handleChange} placeholder='views' />{video.views}
+
+            <button
+                // new onclick function
+                onClick={handleSubmit}
 
             // onClick={() => {
-                // //here we setVideos (setter) by copy of videos (using spread Operator) 
-                // setVideos([...videos, {
-                //     id: videos.length + 1,
-                //     title: 'Demo Js Tutorial',
-                //     name: '1M',
+            // //here we setVideos (setter) by copy of videos (using spread Operator) 
+            // setVideos([...videos, {
+            //     id: videos.length + 1,
+            //     title: 'Demo Js Tutorial',
+            //     name: '1M',
 
-                // }
-                // ]);
+            // }
+            // ]);
             // }}
             >
                 Add Video </button>
