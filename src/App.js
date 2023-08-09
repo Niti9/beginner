7fb968fa947e45c787e994f7e8da6db95632a8aa
Niti9 <nnegi8298@gmail.com>
2023-08-09 21:33:33 +0530

    useReducer, created add video function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import './App.css';
 import videoDB from './data/data.js';
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import AddVideo from './components/AddVideo.js';
 import VideoList from './components/VideoList';
 function App() {
 
   console.log('render App');
-  const [videos, setVideos] = useState(videoDB);
+
+  /**useReducer ki help se hum 
+   * add video function run kar lenge 
+   * by using dispatch aur setVideos 
+   * remove kiya hai isliye use juda 
+   * sab kuch comment karenge warna error
+   *  show hota rahega
+*/
+
+
+
+
+
+  function videoReducer(videos, action) {
+    switch (action.type) {
+      case 'ADD':
+        return [
+          ...videos,
+          //ab ye niche video ki jagah action.payload lenge
+          { ...action.payload, id: videos.length + 1 }
+        ]
+
+
+      default:
+        return videos // atleast 
+    }
+  }
+
+
+  /** Now we will use useReducer
+   * format will be below
+   */
+  // useReducer(reducerName,initialValue)
+  const [videos, dispatch] = useReducer(videoReducer, videoDB);
+
+  // const [videos, setVideos] = useState(videoDB);
   const [editableVideo, setEditableVideo] = useState(null);
 
 
-  //to get data from AddVideo.js with parameter video
   function addVideos(video) {
-    setVideos([
-      ...videos,
-      { ...video, id: videos.length + 1 }
-    ]);
+
+    // Yahan bhi action lagega with type and payload
+    //action: {type:'ADD', payload: video}
+    dispatch({ type: 'ADD', payload: video })
+
+    /** ab isse reducer mein hi use karenge */
+    // setVideos([
+    //   ...videos,
+    //   { ...video, id: videos.length + 1 }
+    // ]);
 
   }
   //to make delete video function
   function deleteVideo(id) {
-    setVideos(videos.filter(video => video.id !== id))
+    // setVideos(videos.filter(video => video.id !== id))
   }
   //to make edit function
   function editVideo(id) {
@@ -33,12 +73,12 @@ function App() {
     iska format hoga (index jo change karni hai , 1 element, video )
     ye splice original state ko change karta hai isliye hum spread operator 
     ki help se copy banakar usmein change karenge*/
-    const index = videos.findIndex(v=>v.id===video.id) //agar variable v ki id 
+    const index = videos.findIndex(v => v.id === video.id) //agar variable v ki id 
     // click karne par kisi video se match hogi to uska index humein mil jaayega
     const newVideos = [...videos]
-    newVideos.splice(index,1,video)
-    
-    setVideos(newVideos);
+    newVideos.splice(index, 1, video)
+
+    // setVideos(newVideos);
   }
 
 
